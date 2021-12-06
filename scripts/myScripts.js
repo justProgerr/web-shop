347e6f67ef349b8682ddad9f19f0c0ef37e8d65e
justProger <just_proger@list.ru>
2021-12-06 11:06:00 +0300

    added adaptive

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,14 +1,24 @@
-let menu = document.querySelector('#main__menu');
-let booster = document.querySelector('#booster');
-let close = document.querySelector('#close');
+let menu = document.getElementById('main__menu');
+let burger = document.getElementById('burger');
+let close = document.getElementById('close');
+let regIcon = document.getElementById('regIcon');
+let cartIcon = document.getElementById('cartIcon');
+let headerRight = document.querySelector('.header__right');
 
-booster.addEventListener('click', function(event) {
+burger.addEventListener('click', function(event) {
     event.preventDefault();
 
     if (menu.style.display == 'none') {
         menu.style.display = 'block';
+        regIcon.classList.add('reg-icon--active');
+        cartIcon.classList.add('cart-icon--active');
+        headerRight.classList.add('header__right-active');
+
     } else {
         menu.style.display = 'none';
+        regIcon.classList.remove('reg-icon--active');
+        cartIcon.classList.remove('cart-icon--active');
+        headerRight.classList.remove('header__right-active');
     };
 });
 
@@ -16,4 +26,7 @@ close.addEventListener('click', function(event) {
     event.preventDefault();
 
     menu.style.display = 'none';
+    regIcon.classList.remove('reg-icon--active');
+    cartIcon.classList.remove('cart-icon--active');
+    headerRight.classList.remove('header__right-active');
 });
